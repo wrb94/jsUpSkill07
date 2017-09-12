725865dd9438b59174d82aba9208721f895553a7
wrb94 <209150@student.pwr.edu.pl>
2017-09-12 20:06:37 +0200

    part 4 done

--- koans/04_currying.js
+++ koans/04_currying.js
@@ -3,7 +3,15 @@ describe("Currying", () => {
     /********************* YOUR IMPLEMENTATION *********************/
 
     // curry :: (* → a) → (* → a)
-
+    function curry(fn) {
+      return function curried(...outerArgs) {
+        if (outerArgs.length < fn.length)
+          return function (...innerArgs) {
+            return curried(...outerArgs.concat(innerArgs))
+          }
+        return fn(...outerArgs)
+      }
+    }
     /***************************************************************/
 
     test("curried function returns a function after applying a first argument", () => {
@@ -21,11 +29,11 @@ describe("Currying", () => {
     // BONUS:
     //
     // HINT: Function.prototype.length specifies function arity
-    //
-    // test("curried function's arguments can be applied within a first call", () => {
-    //   const sum = (a, b) => a + b;
-    //   const curriedSum = curry(sum);
-    //   expect(curriedSum(2, 3)).toEqual(5);
-    // });
+
+    test("curried function's arguments can be applied within a first call", () => {
+      const sum = (a, b) => a + b;
+      const curriedSum = curry(sum);
+      expect(curriedSum(2, 3)).toEqual(5);
+    });
   });
 });
